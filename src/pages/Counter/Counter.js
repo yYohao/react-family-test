9755328a4212e283f225815fa4845e6040c587d9
yYohao <429747705@qq.com>
2018-10-12 23:10:51 +0800

    redux-first-time

---
+++ src/pages/Counter/Counter.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+export default class Counter extends Component{
+    render() {
+        return (
+            <div>
+                <div>当前计数为：</div>
+                <button onClick={() => {
+                    console.log("调用自增函数")
+                }}>自增</button>
+                <button onClick={() => {
+                    console.log("调用自减函数")
+                }}>自减</button>
+                <button onClick={() => {
+                    console.log("调用重置函数")
+                }}>重置</button>
+            </div>
+        )
+    }
+}
